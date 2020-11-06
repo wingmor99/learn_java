50442a0e6984d0bfadc580906f6435748fc8d9f0
linlin <wingmor51@gmail.com>
2020-11-06 14:05:51 +0800

    线程交互，wait， notify
    https://how2j.cn/k/thread/thread-threadpool/357.html

--- src/charactor/SimpleHero.java
+++ src/charactor/SimpleHero.java
@@ -36,12 +36,26 @@ public class SimpleHero {
         }
     }
 
-    public void hurt() {
+    public synchronized void hurt() {
+        if (hp == 1) {
+            try {
+                // 该线程释放
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         this.hp = hp - 1;
+        System.out.printf("%s 减血1点,减少血后，%s的血量是%.0f%n", name, name, hp);
+
+
     }
 
-    public void recover() {
+    public synchronized void recover() {
         this.hp = hp + 1;
+        System.out.printf("%s 回血1点,增加血后，%s的血量是%.0f%n", name, name, hp);
+        // 通知那些等待在this对象上的线程，可以醒过来了，如第20行，等待着的减血线程，苏醒过来
+        this.notify();
     }
 
 
--- src/multiplethread/TestThread.java
+++ src/multiplethread/TestThread.java
@@ -146,6 +146,7 @@ public class TestThread {
 
         Thread t6 = new Thread(){
 
+            @Override
             public void run(){
                 try {
                     System.out.println( now()+" t6 线程已经运行");
@@ -164,6 +165,52 @@ public class TestThread {
         };
         t6.setName(" t6");
 //        t6.start();
+
+        gareen.hp=100;
+        Thread t7 = new Thread(){
+            @Override
+            public void run(){
+                while(true){
+
+                    //因为减血更快，所以盖伦的血量迟早会到达1
+                    //使用while循环判断是否是1，如果是1就不停的循环
+                    //直到加血线程回复了血量
+//                    while(gareen.hp==1){
+//                        continue;
+//                    }
+
+                    gareen.hurt();
+                    System.out.printf("t1 为%s 减血1点,减少血后，%s的血量是%.0f%n",gareen.name,gareen.name,gareen.hp);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+        t7.start();
+
+        Thread t8 = new Thread(){
+            @Override
+            public void run(){
+                while(true){
+                    gareen.recover();
+                    System.out.printf("t2 为%s 回血1点,增加血后，%s的血量是%.0f%n",gareen.name,gareen.name,gareen.hp);
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        };
+        t8.start();
 
 
 
