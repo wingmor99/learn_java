1a4d2c472e60ece66347971c61f4d68e434a1393
linlin <wingmor51@gmail.com>
2020-11-09 16:53:09 +0800

    按键监听， Button

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -1,21 +1,32 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TestGUI {
     public static void main(String[] args) {
         JFrame f = new JFrame("LOL");
-
         f.setSize(400, 300);
-
-        f.setLocation(200, 200);
-
+        f.setLocation(580, 200);
         f.setLayout(null);
 
-        JButton b = new JButton("一键秒基地挂");
-        
-        b.setBounds(50, 50, 280, 30);
+        final JLabel l = new JLabel();
+        ImageIcon i = new ImageIcon("resource/shana.png");
+        l.setIcon(i);
+        l.setBounds(50, 50, i.getIconWidth(), i.getIconHeight());
 
+        JButton b = new JButton("隐藏图片");
+        b.setBounds(150, 200, 100, 30);
+        // 按键增加监听
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                l.setVisible(false);
+            }
+        });
+
+        f.add(l);
         f.add(b);
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
