2ed3e4dc1a6b4f553f4b592c73ad959f2e5b6f68
linlin <wingmor51@gmail.com>
2020-11-09 17:51:17 +0800

    JFrame, JDialog
    JFrame: 窗口带最大最小化
    JDialog: 对话框不带最大最小

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -6,32 +6,25 @@ import java.util.Random;
 
 public class TestGUI {
     public static void main(String[] args) {
+//        JFrame f = new JFrame("LOL");
+//        f.setSize(400, 300);
+//        f.setLocation(200, 200);
+//        f.setLayout(null);
+//        JButton b = new JButton("一键秒水晶");
+//        b.setBounds(50, 50, 280, 30);
+//
+//        f.add(b);
+//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        f.setVisible(true);
+        JDialog d = new JDialog();
+        d.setTitle("LOL");
+        d.setSize(400, 300);
+        d.setLocation(200, 200);
+        d.setLayout(null);
 
-        JFrame f = new JFrame("LoL");
-        f.setSize(800, 600);
-        f.setLocationRelativeTo(null);
-        f.setLayout(null);
-
-        final JLabel l = new JLabel();
-
-        ImageIcon i = new ImageIcon("resource/shana_heiheihei.png");
-        l.setIcon(i);
-        l.setBounds(375, 275, i.getIconWidth(), i.getIconHeight());
-
-        f.add(l);
-        l.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseEntered(MouseEvent e) {
-                Random r = new Random();
-                int x = r.nextInt(f.getWidth() - l.getWidth());
-                int y = r.nextInt(f.getHeight() - l.getHeight());
-                l.setLocation(x, y);
-                System.out.println("mouse entered");
-            }
-        });
-
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        f.setVisible(true);
+        JButton b = new JButton("一键秒水晶");
+        b.setBounds(50, 50, 280, 30);
+        d.add(b);
+        d.setVisible(true);
     }
 }
