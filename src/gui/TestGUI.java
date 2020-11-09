7df025cbd011a8cef79d530c54bd8a8a634f3495
linlin <wingmor51@gmail.com>
2020-11-09 17:27:27 +0800

    鼠标监听

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -1,55 +1,57 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.*;
+import java.util.Random;
 
 public class TestGUI {
     public static void main(String[] args) {
 
         JFrame f = new JFrame("LoL");
-        f.setSize(400, 300);
-        f.setLocation(580, 200);
+        f.setSize(800, 600);
+        f.setLocationRelativeTo(null);
         f.setLayout(null);
 
         final JLabel l = new JLabel();
 
-        ImageIcon i = new ImageIcon("resource/shana.png");
+        ImageIcon i = new ImageIcon("resource/shana_heiheihei.png");
         l.setIcon(i);
-        l.setBounds(50, 50, i.getIconWidth(), i.getIconHeight());
+        l.setBounds(375, 275, i.getIconWidth(), i.getIconHeight());
 
-        // 增加键盘监听
-        f.addKeyListener(new KeyListener() {
-
-            // 键被弹起
+        f.add(l);
+        l.addMouseListener(new MouseListener() {
             @Override
-            public void keyReleased(KeyEvent e) {
-
-                // 39代表按下了 “右键”
-                if (e.getKeyChar() == 'd') {
-                    // 图片向右移动 （y坐标不变，x坐标增加）
-                    l.setLocation(l.getX() + 50, l.getY());
-                }
-                System.out.println(e.getKeyChar());
-            }
-
-            //键被按下
-            @Override
-            public void keyPressed(KeyEvent e) {
-                // TODO Auto-generated method stub
+            public void mouseClicked(MouseEvent e) {
 
             }
 
-            // 一个按下弹起的组合动作
             @Override
-            public void keyTyped(KeyEvent e) {
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                Random r = new Random();
+
+                int x = r.nextInt(f.getWidth() - l.getWidth());
+                int y = r.nextInt(f.getHeight() - l.getHeight());
+
+                l.setLocation(x, y);
+                System.out.println("mouse entered");
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
 
             }
         });
-
-        f.add(l);
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
