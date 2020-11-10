198d38103200b95b5ffcaafb2b95b66f436b3c7d
linlin <wingmor51@gmail.com>
2020-11-10 10:30:34 +0800

    锁定框
    .setResizable(false)

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -9,21 +9,15 @@ public class TestGUI {
         JFrame f = new JFrame("外部窗体");
         f.setSize(800, 600);
         f.setLocation(100, 100);
-
-        // 实例化JDialog
-        JDialog d = new JDialog(f);
-        //设置为模态
-        d.setModal(true);
-
-        d.setTitle("模态的对话框");
-        d.setSize(400, 300);
-        d.setLocation(200, 200);
-        d.setLayout(null);
+        f.setLayout(null);
         JButton b = new JButton("一键秒基地");
         b.setBounds(50, 50, 280, 30);
-        d.add(b);
+        f.add(b);
+
+        //窗体大小不可变
+        f.setResizable(false);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         f.setVisible(true);
-        d.setVisible(true);
     }
 }
