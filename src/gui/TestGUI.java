ceb6d4b4e48ee109eb7f5f1ec2a783fb68156702
linlin <wingmor51@gmail.com>
2020-11-10 10:39:39 +0800

    BorderLayout 东南西北中

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -1,7 +1,7 @@
 package gui;
 
 import javax.swing.*;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
 
@@ -13,18 +13,23 @@ public class TestGUI {
         // 绝对定位，所有组件都要指定位置和大小
         f.setLayout(null);
 
-        // 设置布局器为FlowLayerout
-        // 容器上的组件水平摆放
-        f.setLayout(new FlowLayout());
+        // 设置布局器为BorderLayerout
+        // 容器上的组件按照上北下南左西右东中的顺序摆放
+        f.setLayout(new BorderLayout());
 
-        JButton b1 = new JButton("英雄1");
-        JButton b2 = new JButton("英雄2");
-        JButton b3 = new JButton("英雄3");
+        JButton b1 = new JButton("洪七");
+        JButton b2 = new JButton("段智兴");
+        JButton b3 = new JButton("欧阳锋");
+        JButton b4 = new JButton("黄药师");
+        JButton b5 = new JButton("周伯通");
 
-        // 加入到容器即可，无需单独指定大小和位置
-        f.add(b1);
-        f.add(b2);
-        f.add(b3);
+        // 加入到容器的时候，需要指定位置
+        f.add(b1, BorderLayout.NORTH);
+        f.add(b2, BorderLayout.SOUTH);
+        f.add(b3, BorderLayout.WEST);
+        f.add(b4, BorderLayout.EAST);
+        f.add(b5, BorderLayout.CENTER);
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         f.setVisible(true);
