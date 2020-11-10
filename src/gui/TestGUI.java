08366b8b7e3a753d61f1a55786bb47be88bd4f82
linlin <wingmor51@gmail.com>
2020-11-10 10:37:48 +0800

    FlowLayout布局

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.FlowLayout;
 import java.awt.event.*;
 import java.util.Random;
 
@@ -11,21 +12,19 @@ public class TestGUI {
         f.setLocation(200, 200);
         // 绝对定位，所有组件都要指定位置和大小
         f.setLayout(null);
-        // 指定位置和大小
-        JButton b1 = new JButton("英雄1");
-        b1.setBounds(50, 50, 80, 30);
-        JButton b2 = new JButton("英雄2");
-        b2.setBounds(150, 50, 80, 30);
-        JButton b3 = new JButton("英雄3");
-        b3.setBounds(250, 50, 80, 30);
-        // 没有指定位置和大小，不会出现在容器上
-        JButton b4 = new JButton("英雄3");
 
+        // 设置布局器为FlowLayerout
+        // 容器上的组件水平摆放
+        f.setLayout(new FlowLayout());
+
+        JButton b1 = new JButton("英雄1");
+        JButton b2 = new JButton("英雄2");
+        JButton b3 = new JButton("英雄3");
+
+        // 加入到容器即可，无需单独指定大小和位置
         f.add(b1);
         f.add(b2);
         f.add(b3);
-        // b4没有指定位置和大小，不会出现在容器上
-        f.add(b4);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         f.setVisible(true);
