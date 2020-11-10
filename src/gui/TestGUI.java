15418b780ac76a6987f05d40c59fec0873677ede
linlin <wingmor51@gmail.com>
2020-11-10 10:26:41 +0800

    模态JDialog
    对话框模态时，父框关不了

--- src/gui/TestGUI.java
+++ src/gui/TestGUI.java
@@ -6,25 +6,24 @@ import java.util.Random;
 
 public class TestGUI {
     public static void main(String[] args) {
-//        JFrame f = new JFrame("LOL");
-//        f.setSize(400, 300);
-//        f.setLocation(200, 200);
-//        f.setLayout(null);
-//        JButton b = new JButton("一键秒水晶");
-//        b.setBounds(50, 50, 280, 30);
-//
-//        f.add(b);
-//        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        f.setVisible(true);
-        JDialog d = new JDialog();
-        d.setTitle("LOL");
+        JFrame f = new JFrame("外部窗体");
+        f.setSize(800, 600);
+        f.setLocation(100, 100);
+
+        // 实例化JDialog
+        JDialog d = new JDialog(f);
+        //设置为模态
+        d.setModal(true);
+
+        d.setTitle("模态的对话框");
         d.setSize(400, 300);
         d.setLocation(200, 200);
         d.setLayout(null);
-
-        JButton b = new JButton("一键秒水晶");
+        JButton b = new JButton("一键秒基地");
         b.setBounds(50, 50, 280, 30);
         d.add(b);
+
+        f.setVisible(true);
         d.setVisible(true);
     }
 }
